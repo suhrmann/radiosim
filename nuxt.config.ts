5d794a59b61901dc03c726075ed7c2b858cf6fd9
Lucas Kurz <lucas.kurz@ubiqa.eu>
2024-11-14 23:48:46 +0100

    Added nuxt/fonts and choose some fonts for the app

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
 
     compatibilityDate: '2024-04-03',
     devtools: {enabled: true},
-    modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/icon'],
+    modules: ['@nuxtjs/tailwindcss', '@pinia/nuxt', '@nuxt/icon', '@nuxt/fonts'],
     ssr: false,
 
     app: {
